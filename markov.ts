01df44dc7f7715ff20a5b3bc53c8fe2636fed308
Gerard Keiser <gerardkeiser@gmail.com>
2022-08-20 16:15:19 -0500

    makeChains function (mostly) working

--- markov.ts
+++ markov.ts
@@ -1,6 +1,5 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
   words: string[];
 
@@ -8,7 +7,7 @@ class MarkovMachine {
 
   constructor(text: string) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -18,9 +17,12 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    // this.words.reduce((chain, word, i) => {
+    //   return chain.word
+    //     ? chain[word].push(words[i + i])
+    //     : (chain[word] = [words[i + 1]]);
+    // }, {}: Chain);
   }
-
 
   /** return random text from chains */
 
@@ -28,3 +30,20 @@ class MarkovMachine {
     // TODO
   }
 }
+
+interface Chain {
+  [index: string]: string[];
+}
+
+function makeChains(words: string[]): Chain {
+  return words.reduce((chain, word, i, words) => {
+    chain[word]
+      ? chain[word].push(words[i + 1])
+      : (chain[word] = [words[i + 1]]);
+    return chain;
+  }, <Chain>{});
+}
+
+console.log(makeChains(["the", "cat", "in", "the", "hat"]));
+
+export { MarkovMachine };
