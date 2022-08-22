98345b1762aad5f22ccff164cfe00041de9cf341
Gerard Keiser <gerardkeiser@gmail.com>
2022-08-22 17:46:27 -0500

    Update tests for Either

--- makeText.test.ts
+++ makeText.test.ts
@@ -2,14 +2,14 @@ const { main, cat, webCat } = require("./makeText");
 
 describe("cat", () => {
   test("cat returns file as string", async () => {
-    const text = await cat("eggs.txt");
+    const text = (await cat("eggs.txt")).right;
     expect(text).toContain("Sam I am");
   });
 });
 
 describe("webCat", () => {
   test("webCat returns webpage as string", async () => {
-    const text = await webCat("https://makeheadlines.gerardkeiser.com");
+    const text = (await webCat("https://makeheadlines.gerardkeiser.com")).right;
     expect(text).toContain("Make Headlines");
   });
 });
--- markov.test.ts
+++ markov.test.ts
@@ -2,14 +2,14 @@ const { makeText, makeChains } = require("./markov");
 
 describe("makeText", () => {
   test("makeText stops after given numbers of words", () => {
-    const chain = makeChains(["the", "cat", "the"]);
-    expect(makeText(chain, 5).split(" ").length).toEqual(5); // The array has a trailing empty string;
+    const chain = makeChains(["the", "cat", "the"]).right;
+    expect(makeText(chain, 5).right.split(" ").length).toEqual(5); // The array has a trailing empty string;
   });
 });
 
 describe("makeChains", () => {
   test("makeChains makes properly makes a simple chain", () => {
-    const chain = makeChains(["the", "cat", "the"]);
+    const chain = makeChains(["the", "cat", "the"]).right;
     expect(chain).toEqual({ the: ["cat"], cat: ["the"] });
   });
 });
